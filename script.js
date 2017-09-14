4e1ddca5526b80e718d30328fc1a51ec5dc711e8
marinhorafael <rafaelmarinho@id.uff.br>
2017-09-14 18:30:11 -0300

    Add files via upload
    Versões finais do trabalho 2 de Desenvolvimento Web
    Universidade Federal Fluminense

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-			function valida() {
-				var i;
-				var j;
-				var msgerr;
-				var x = document.cadastro;
-				var txt = '';
-				
-				for (i = 0; i < x.length; i++) {
-					if(x.elements[i].value == ''){
-						j = document.forms[0][i].id;
-						msgerr = (j + ' é vazio!\n');
-						txt = txt + msgerr;
-					}
-				}
-				
-				if (txt != ''){
-					alert(txt);
-					return false;
-				}
-			}
+function valida() {
+	var i;
+	var j;
+	var msgerr;
+	var x = document.cadastro;
+	var txt = '';
+	
+	for (i = 0; i < x.length; i++) {
+		if(x.elements[i].value == ''){
+			j = document.forms[0][i].name;
+			msgerr = ('Preencha o campo ' + j + '. Não é permitida a entrada vazia!\n' );
+			txt = txt + msgerr;
+		}
+	}
+	
+	if (txt != ''){
+		alert(txt);
+		return false;
+	}
+}
